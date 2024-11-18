23e96906872e88b6b8568e1cae7109cc226001f7
Carlos Giovano <carlosgiovano@outlook.com>
2024-11-17 23:07:05 -0300

    Adicionado modelo para atividade

--- src/app/models/atividade.ts
+++ src/app/models/atividade.ts
@@ -1,7 +1,8 @@
 export interface Atividade {
     id: number;
     descricao: string;
-    data_criacao: Date;
-    data_entrega: Date;
+    data_inicio: string;
+    data_fim: string;
     valor: number;
+    numero_questoes: number;
 }
