5af5f31482aefcaa05bd33afb45884db0f2e5cc8
Carlos Giovano <carlosgiovano@outlook.com>
2024-11-17 23:06:05 -0300

    lógica para a tela de gerenciamento iniciada (incompleto)

--- src/app/atividade/gerenciar-atividade/gerenciar-atividade.component.ts
+++ src/app/atividade/gerenciar-atividade/gerenciar-atividade.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { UrlBaseService } from '../../UrlBaseService';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-gerenciar-atividade',
   templateUrl: './gerenciar-atividade.component.html',
   styleUrl: './gerenciar-atividade.component.css'
 })
-export class GerenciarAtividadeComponent {
+export class GerenciarAtividadeComponent implements OnInit {
+  idAtividade: number;
+  constructor(private httpClient: HttpClient, private urlBase: UrlBaseService, private activatedRoute: ActivatedRoute, private router: Router) {}
+  
+  ngOnInit(): void {
+    this.idAtividade = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    console.log(this.idAtividade);
+  }
+
 
 }
