c5ce45634d455a729980f6cd52b8a440abf7607e
Carlos Giovano <carlosgiovano@outlook.com>
2024-11-17 23:01:19 -0300

    Adicionadas funcionalidades básicas para a tela de atividades

--- src/app/atividade/atividade.component.ts
+++ src/app/atividade/atividade.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Atividade } from '../models/atividade';
+import { UrlBaseService } from '../UrlBaseService';
 
 @Component({
   selector: 'app-atividade',
@@ -8,11 +9,16 @@ import { Atividade } from '../models/atividade';
   styleUrl: './atividade.component.css'
 })
 export class AtividadeComponent implements OnInit {
-    atividades: Atividade[] = [];
+  atividades: Atividade[] = [];
 
-  constructor(private httpClient: HttpClient, ) {}
+  constructor(private httpClient: HttpClient, private urlBase: UrlBaseService) { }
 
   ngOnInit(): void {
-    
+    this.httpClient.get<Atividade[]>(`${this.urlBase.getUrl()}/atividade`).subscribe((dados) => this.atividades = dados);
+  }
+
+  Excluir(id: number) {
+    console.log(id);
+    this.httpClient.delete(`${this.urlBase.getUrl()}/atividade/${id}`).subscribe(() => this.httpClient.get<Atividade[]>(`${this.urlBase.getUrl()}/atividade`).subscribe((dados) => this.atividades = dados));
   }
 }
